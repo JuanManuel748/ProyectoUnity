39c98ea53b6ed02179f2a78ea661ac4af021958d
Franfuu <franperezruiz10@gmail.com>
2024-10-31 17:59:00 +0100

    Juego arreglado, animacion Idle hecha

---
+++ MDAlien/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Rigidbody2D rb;
+    bool isFacingRight = true;
+    
+    [Header("Movement")]
+	public float moveSpeed = 5f;
+	float horizontalMovement;
+
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+    
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2 (0.5f, 0.05f);
+    public LayerMask groundLayer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+        flip();
+    }
+	
+	public void Move(InputAction.CallbackContext context) {
+		horizontalMovement = context.ReadValue<Vector2>().x;
+	}
+
+    public void Jump(InputAction.CallbackContext context) {
+		if (isGrounded()) {
+            if (context.performed) {
+                rb.velocity=new Vector2(rb.velocity.x, jumpPower);
+            }
+		}
+    }
+
+    private bool isGrounded() {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer)) {
+            return true;
+        }
+		return false;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+
+    void flip() {
+        if ((isFacingRight && horizontalMovement < 0) || (!isFacingRight && horizontalMovement > 0)) {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+}
